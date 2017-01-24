2d3690862f5d900efd01381e81e7cf4cb84a8ca5
Felipe Resende <feliperesendehq@outlook.com>
2017-01-24 17:12:40 -0200

    Prefab Ryu Reconfigured
    Any bugs was fixe and gitgnore updated

--- Assets/Script/scene/Round.cs
+++ Assets/Script/scene/Round.cs
@@ -6,10 +6,10 @@ using UnityEngine.SceneManagement;
 public class Round : MonoBehaviour {
 
 	public bool battleStart;
-	public GameObject player1;
-	public GameObject player2;
-	public Player character1;
-	public Player character2;
+	public GameObject playerObject1;
+	public GameObject playerObject2;
+	public Player player1;
+	public Player player2;
 	public Controller controler1;
 	public Controller controler2;
 	public Transform startPoint1;
@@ -20,50 +20,50 @@ public class Round : MonoBehaviour {
 	public 
 	// Use this for initialization
 	void Start () {
-		GameObject playerCharacter1 = Instantiate (player1, startPoint1.position, Quaternion.identity) as GameObject;
-		GameObject playerCharacter2 = Instantiate (player2, startPoint2.position, Quaternion.identity) as GameObject;
-		character1 = playerCharacter1.GetComponent<Player> ();
-		character2 = playerCharacter2.GetComponent<Player> ();
-		character1.controller = controler1;
-		character2.controller = controler2;
-		character1.controller.enable = false;
-		character2.controller.enable = false;
-		controler1.player = character1;
-		controler2.player = character2;
-		character1.id = 1;
-		character2.id = 2;
-		character1.enemy = playerCharacter2;
-		character2.enemy = playerCharacter1;
-		playergui1.player = character1;
-		playergui2.player = character2;
+		GameObject playerCharacter1 = Instantiate (playerObject1, startPoint1.position, Quaternion.identity) as GameObject;
+		GameObject playerCharacter2 = Instantiate (playerObject2, startPoint2.position, Quaternion.identity) as GameObject;
+		player1 = playerCharacter1.GetComponent<Player> ();
+		player2 = playerCharacter2.GetComponent<Player> ();
+		player1.controller = controler1;
+		player2.controller = controler2;
+		player1.controller.enable = false;
+		player2.controller.enable = false;
+		controler1.player = player1;
+		controler2.player = player2;
+		player1.id = 1;
+		player2.id = 2;
+		player1.enemy = playerCharacter2;
+		player2.enemy = playerCharacter1;
+		playergui1.player = player1;
+		playergui2.player = player2;
 		Invoke ("StartRound", 3);
 		battleStart = true;
 	}
 
 	void Update(){
-		if (character1.life <= 0 && battleStart) {
+		if (player1.life <= 0 && battleStart) {
 			Debug.Log ("Player 2 Ganhou");
-			character1.controller.enable = false;
-			character2.controller.enable = false;
+			player1.controller.enable = false;
+			player2.controller.enable = false;
 			interfaceAnimator.Play ("Player1Win");
-			character1.anim.SetBool ("OnDead", true);
+			player1.anim.SetBool ("OnDead", true);
 			battleStart = false;
 			Invoke ("ReloadScene", 3);
 		}
-		if(character2.life <= 0 && battleStart) {
+		if(player2.life <= 0 && battleStart) {
 			Debug.Log ("Player 1 Ganhou");
-			character1.controller.enable = false;
-			character2.controller.enable = false;
+			player1.controller.enable = false;
+			player2.controller.enable = false;
 			interfaceAnimator.Play ("Player2Win");
-			character2.anim.SetBool ("OnDead", true);
+			player2.anim.SetBool ("OnDead", true);
 			battleStart = false;
 			Invoke ("ReloadScene", 5);
 		}
 	}
 	void StartRound(){
 		battleStart = true;
-		character1.controller.enable = true;
-		character2.controller.enable = true;
+		player1.controller.enable = true;
+		player2.controller.enable = true;
 	}
 	void ReloadScene(){
 		SceneManager.LoadScene("Main");
